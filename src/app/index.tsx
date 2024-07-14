78ee6381a5668c50dec4f41a0af35e841e71981c
baechan <asdfjklddd@naver.com>
2024-07-14 23:03:20 +0900

    tanstack query Provider 적용

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,6 +5,8 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import '@/app/index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { RecoilRoot } from 'recoil';
 
 const App: React.FC = () => {
@@ -29,11 +31,26 @@ const container = document.getElementById('root');
 if (container) {
   const root = createRoot(container);
 
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+        retry: false,
+      },
+    },
+  });
+
+  const isDevtoolsOpen =
+    import.meta.env.VITE_IS_TANSTACK_QUERY_DEVTOOLS_OPEN === 'true';
+
   root.render(
     <React.StrictMode>
-      <RecoilRoot>
-        <App />
-      </RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <App />
+        </RecoilRoot>
+        <ReactQueryDevtools initialIsOpen={isDevtoolsOpen} />
+      </QueryClientProvider>
     </React.StrictMode>,
   );
 }
